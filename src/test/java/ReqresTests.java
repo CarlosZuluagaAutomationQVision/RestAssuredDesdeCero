489ba87810171884d4ef0d0991e5b67345b7963b
Carlos Andrés Zuluaga Gómez <czuluaga@qvision.us>
2020-11-22 18:11:42 -0500

    Parte 2 Taller - Codigo más Limpio - Metodos Put, Patch, Delete

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -1,28 +1,41 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReqresTests {
 
+  @Before
+  public void setup() {
+    RestAssured.baseURI = "https://reqres.in";
+    RestAssured.basePath = "/api";
+    RestAssured
+        .filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
+    RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON)
+        .build();
+  }
+
   @Test
   public void postLoguinTest() {
-
-    RestAssured
-        .given()
-        .log().all()
-        .contentType(ContentType.JSON)
+    given()
         .body("{\n"
             + "    \"email\": \"eve.holt@reqres.in\",\n"
             + "    \"password\": \"cityslicka\"\n"
             + "}")
-        .post("https://reqres.in/api/login")
+        .post("login")
         .then()
-        .log().all()
-        .statusCode(200)
+        .statusCode(HttpStatus.SC_OK)
         .body("token", notNullValue())
         .body("token", equalTo("QpwL5tke4Pnpja7X4"));
 
@@ -30,13 +43,10 @@ public class ReqresTests {
 
   @Test
   public void GetSingleUserTest() {
-    RestAssured
-        .given()
-        .log().all()
-        .get("https://reqres.in/api/users/2")
+    given()
+        .get("users/2")
         .then()
-        .log().all()
-        .statusCode(200)
+        .statusCode(HttpStatus.SC_OK)
         .body("data.id", equalTo(2))
         .body("data.first_name", equalToIgnoringCase("janet"))
         .body("support.text", equalToIgnoringCase(
@@ -45,21 +55,56 @@ public class ReqresTests {
 
   @Test
   public void postCreateTest() {
-
-    RestAssured
-        .given()
-        .log().all()
-        .contentType(ContentType.JSON)
+    given()
         .body("{\n"
             + "    \"name\": \"Carlos\",\n"
             + "    \"job\": \"Automatizador\"\n"
             + "}")
-        .post("https://reqres.in/api/users")
+        .post("users")
         .then()
-        .log().all()
-        .statusCode(201)
+        .statusCode(HttpStatus.SC_CREATED)
         .body("name", equalTo("Carlos"))
         .body("job", equalTo("Automatizador"));
-
   }
+
+  @Test
+  public void deleteUserTest() {
+    given()
+        .delete("users/2")
+        .then()
+        .statusCode(HttpStatus.SC_NO_CONTENT);
+  }
+
+  @Test
+  public void patchUserTest() {
+    String nombreActualizado = given()
+        .when()
+        .body("{\n"
+            + "    \"name\": \"Carlos\",\n"
+            + "    \"job\": \"Automatizador\"\n"
+            + "}")
+        .patch("users/2")
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .extract().jsonPath().getString("name");
+
+    assertThat(nombreActualizado, equalTo("Carlos"));
+  }
+
+  @Test
+  public void putUserTest() {
+    String trabajoActualizado = given()
+        .when()
+        .body("{\n"
+            + "    \"name\": \"Carlos\",\n"
+            + "    \"job\": \"Automatizador\"\n"
+            + "}")
+        .put("users/2")
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .extract().jsonPath().getString("job");
+
+    assertThat(trabajoActualizado, equalTo("Automatizador"));
+  }
+
 }
